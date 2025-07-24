d83bd15058a08f6c2a1433e90402b49424abf6f1
Walby12 <alby09.bonf@gmail.com>
2025-07-24 18:10:23 +0200

    Implemented the server

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,46 @@
-use std::env;
-use std::path::PathBuf;
+use std::fs;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
-#[derive(Debug, Clone)]
-enum Tokens {
-    Ls,
-    Cat(String),
+fn make_json(path: &str) {
+    fs::File::create(path).unwrap();
 }
 
-fn tokenize(src: &str) -> Vec<Tokens> {
-    let mut toks: Vec<Tokens> = Vec::new();
-    let code: Vec<&str> = src.split_whitespace().collect();
+fn append_json(path: &str, context: &str, content: &str) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(path)
+        .unwrap();
+    
+    let json_to_write = format!("{context} {{\n\t{content}\n}}\n");
 
-    let mut i = 0;
-
-    while i < code.len() {
-        let c = code[i];
-        
-        match c {
-            "ls" => {
-                toks.push(Tokens::Ls);
-                i += 1;
-            }
-            "cat" => {
-                toks.push(Tokens::Cat(String::from(code[i+1])));
-                i += 2;
-            }
-            _ => {
-                println!("ERROR: Unrecogized option: {}, in {}", c, src);
-                assert!(false);
-            }
-        }
-    }
-    toks
-}
-
-fn parse(toks: Vec<Tokens>) {
-    let mut i = 0;
-
-    while i < toks.len() {
-        let t = toks[i].clone();
-
-        match t {
-            Tokens::Ls => {
-                match env::current_dir() {
-                    Ok(path) => println!("{}", path.display()),
-                    Err(e) => println!("ERROR: Could not get the current_dir: {}", e),
-                }
-                i += 1;
-            }
-            _ => {
-                todo!();
-                i += 1;
-            }
-        }
+    let ms = file.write_all(json_to_write.as_bytes());
+    match ms {
+        Ok(_) => println!("Ok: succesfuly writed to file"),
+        Err(e) => eprintln!("Error: Could not write to file, {}", e),
     }
 }
 
-fn main() {
-    let toks = tokenize("ls");
-    println!("{:?}", toks);
-    parse(toks);
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let file_name = "messages.json";
+
+    make_json(file_name);
+    append_json(file_name, "walby", "1: hello\n\t2: bye\n");
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
